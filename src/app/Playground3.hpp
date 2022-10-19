26a8c2032d4ae7a891cace4be52c909203c96b7b
lunokIoT <797792+lunokjod@users.noreply.github.com>
2022-10-19 08:06:59 +0200

    forgotten Playground

---
+++ src/app/Playground3.hpp
@@ -0,0 +1,31 @@
+#ifndef __LUNOKIOT__PLAYGROUND3_APP__
+#define __LUNOKIOT__PLAYGROUND3_APP__
+
+#include <Arduino.h>
+#include <LilyGoWatch.h>
+#include "../system/Application.hpp"
+
+#include "../UI/widgets/ButtonImageXBMWidget.hpp"
+#include "../UI/widgets/SwitchWidget.hpp"
+#include "../UI/widgets/GaugeWidget.hpp"
+
+class PlaygroundApplication3: public LunokIoTApplication {
+    private:
+        unsigned long nextRedraw=0;
+        unsigned long timeTick=0;
+        unsigned long fixTimeTimeout=-1;
+        const unsigned long SetTimeTimeout=5000; 
+        bool showTime = true;
+        bool timeIsSet = false;
+        int hour=0;
+        int minute=0;
+    public:
+        GaugeWidget * hourGauge = nullptr;
+        GaugeWidget * minuteGauge = nullptr;
+
+        PlaygroundApplication3();
+        ~PlaygroundApplication3();
+        bool Tick();
+};
+
+#endif
