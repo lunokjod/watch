805a8336e19d2ed9b7c3b2e189e79f2264eba1dc
lunokIoT <797792+lunokjod@users.noreply.github.com>
2022-10-22 19:14:08 +0200

    fade in lights

--- src/UI/BootSplash.hpp
+++ src/UI/BootSplash.hpp
@@ -1,12 +1,6 @@
 #ifndef __LUNOKIOT__BOOT_SCREEN__
 #define __LUNOKIOT__BOOT_SCREEN__
 
-#include <Arduino.h>
-#include <LilyGoWatch.h>
-
-#include "../static/img_splash.c"
-
 void SplashAnnounce();
-//void LaunchSplash();
-//void DrawSplashStatus(const char *textBanner);
+void SplashAnnounceEnd();
 #endif
