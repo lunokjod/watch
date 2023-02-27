855b43faed7d50cf1c105202b731511ee6ef717f
lunokIoT <797792+lunokjod@users.noreply.github.com>
2023-02-27 01:13:12 +0100

    openweather service crudely migrated

---
+++ src/app/GestureSettings.hpp
@@ -0,0 +1,38 @@
+//
+//    LunokWatch, a open source smartwatch software
+//    Copyright (C) 2022,2023  Jordi Rubió <jordi@binarycell.org>
+//    This file is part of LunokWatch.
+//
+// LunokWatch is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later 
+// version.
+//
+// LunokWatch is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+// details.
+//
+// You should have received a copy of the GNU General Public License along with 
+// LunokWatch. If not, see <https://www.gnu.org/licenses/>. 
+//
+
+#ifndef __LUNOKIOT__APPLICATION__GESTURESETTINGS__
+#define __LUNOKIOT__APPLICATION__GESTURESETTINGS__
+
+#include "../UI/AppTemplate.hpp"
+#include "../UI/widgets/SwitchWidget.hpp"
+
+class GestureSettings : public TemplateApplication {
+    private:
+        unsigned long nextRefresh=0;
+        SwitchWidget * UseDoubleTapSwitch = nullptr;
+        SwitchWidget * UseLampSwitch = nullptr;
+    public:
+        const char *AppName() override { return "Gesture Settings"; };
+        GestureSettings();
+        ~GestureSettings();
+        bool Tick();
+};
+
+#endif
